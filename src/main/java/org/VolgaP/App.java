2a483da3b2e1cd8d805fe0530df016c9d8803f17
olga <ole4ka2403@gmail.com>
2022-04-24 20:50:56 +0300

    Initial commit

---
+++ src/main/java/org/VolgaP/App.java
@@ -0,0 +1,13 @@
+package org.VolgaP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
